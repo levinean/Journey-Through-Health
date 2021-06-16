4ec7f3b1155b2c681dd5b64ca1c8523d19f92fa3
Andrew Levine <andrew@commure.com>
2021-06-16 16:52:49 -0400

    Added services for images and notes

--- journey_through_health/src/main/java/com/example/journey_through_health/note/NoteRepository.java
+++ journey_through_health/src/main/java/com/example/journey_through_health/note/NoteRepository.java
@@ -1,6 +1,8 @@
 package com.example.journey_through_health.note;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface NoteRepository extends JpaRepository<Note, Long> {
 }
