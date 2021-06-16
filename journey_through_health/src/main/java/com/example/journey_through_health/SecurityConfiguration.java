29213c4e611c6e5a31555dae9205c42f990d733c
Pedro Cuadros <pedro.cuadros@commure.com>
2021-06-16 17:52:15 -0500

    chore: fixing some things

--- journey_through_health/src/main/java/com/example/journey_through_health/SecurityConfiguration.java
+++ journey_through_health/src/main/java/com/example/journey_through_health/SecurityConfiguration.java
@@ -9,6 +9,6 @@ class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+        http.cors().and().csrf().disable().authorizeRequests().anyRequest().permitAll();
     }
 }
