a45b8ecd9129b06b4eedb987475978846ff81370
Andrew Levine <andrew@commure.com>
2021-06-16 17:14:09 -0400

    Added function to find all images and notes based on event id

--- journey_through_health/src/main/java/com/example/journey_through_health/image/ImageRepository.java
+++ journey_through_health/src/main/java/com/example/journey_through_health/image/ImageRepository.java
@@ -1,6 +1,8 @@
 package com.example.journey_through_health.image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface ImageRepository extends JpaRepository<Image, Long> {
+import java.util.List;
 
+interface ImageRepository extends JpaRepository<Image, Long> {
+    List<Image> findByEventId(Long eventId);
 }
