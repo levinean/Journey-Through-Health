cec47e3b7f56a535ff90b3b6dfb615e5f9c9533e
Pedro Cuadros <pedro.cuadros@commure.com>
2021-06-17 12:57:23 -0500

    feat: include search support with elasticsearch

--- journey_through_health/src/main/java/com/example/SecurityConfiguration.java
+++ journey_through_health/src/main/java/com/example/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.journey_through_health;
+package com.example;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
--- journey_through_health/src/main/java/com/example/journey_through_health/event/EventController.java
+++ journey_through_health/src/main/java/com/example/journey_through_health/event/EventController.java
@@ -1,5 +1,7 @@
 package com.example.journey_through_health.event;
 
+import com.example.elasticsearch.event.EventSearchService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,16 +11,12 @@ import java.util.Optional;
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
     private final EventService eventService;
 
-    public EventController(EventService eventService) {
-        this.eventService = eventService;
-    }
-
     @GetMapping("")
-    public ResponseEntity<Object> getAllEvents(@RequestParam(value = "type", defaultValue = "all") String type,
-                                               @RequestParam(value = "priority", defaultValue = "all") String priority) {
+    public ResponseEntity<Object> getAllEvents() {
         return new ResponseEntity<>(eventService.listAll(), HttpStatus.OK);
     }
 
@@ -31,16 +29,4 @@ public class EventController {
                 .map(event -> new ResponseEntity<>(event, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
-
-    @PutMapping("/{id}")
-    public void editEvent(@RequestBody Event newEvent, @PathVariable Long id) {
-        System.out.println("Event edited");
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteEvent(@PathVariable Long id) {
-        System.out.println("Event deleted");
-    }
-
-
 }
